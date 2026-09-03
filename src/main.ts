4f57938843e25003784b01d8fb79fa435fd848ba
ilhamdhiya01 <ulhaqilhamdhiya@gmail.com>
2026-09-03 15:36:25 +0700

    fix: reorder global prefix setting for consistency

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,9 @@ import { Logger } from 'winston';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
 
-  app.setGlobalPrefix('api/v1');
   const logger: Logger = app.get(WINSTON_MODULE_NEST_PROVIDER);
+
+  app.setGlobalPrefix('api/v1');
   app.useLogger(logger);
 
   await app.listen(process.env.PORT ?? 3001);
