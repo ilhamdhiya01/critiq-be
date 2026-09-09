9e9f76bf3f7d68425605b76302d43d50154a3987
ilhamdhiya01 <ulhaqilhamdhiya@gmail.com>
2026-09-09 15:18:46 +0700

    feat: enhance commitlint configuration with custom rules for commit message validation

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,11 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore'],
+    ],
+    'body-max-line-length': [0],
+  },
+};
